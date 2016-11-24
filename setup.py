78fcdf51711004843d6da4e309e3d94d7500692c
Tambet Matiisen <tambet.matiisen@gmail.com>
2016-11-23 16:55:18 -0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(name='minecraft-py',
       version='0.0.2',
       description='Python3 bindings for Malmo',
-      url='https://github.com/tambetm/minecraft-py3',
+      url='https://github.com/tambetm/minecraft-py34-ubuntu1404',
       author='Tambet Matiisen',
       author_email='tambet.matiisen@gmail.com',
       packages=find_packages(),
